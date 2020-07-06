4acd067e3b1a340c7e27251b1f285ef259c90d52
siddhantlohia11 <49112761+siddhantlohia11@users.noreply.github.com>
2020-07-06 17:16:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
 })
 
+app.get("/register", function(req,res){
+   res.sendFile('register.html', {root: __dirname });
+//   res.render("register");
+});
+
+
+app.post("/register", function(req,res){
+//   res.redirect("/secrets");
+   res.sendFile('secrets.html', {root: __dirname });
+});
+
 app.use((req, res) => res.status(404).send({code: '404', message: 'no found'}))
 
 app.listen(port, () => console.log('server started on port', port))
